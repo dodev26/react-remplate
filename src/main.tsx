5a160087af7fa00ef994776a975e00b6befc7790
dopdn <dopdn@bap.jp>
2023-03-14 16:50:15 +0700

    init project

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,26 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import AppProvider from './contexts/app.context'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { QueryClientProvider, QueryClient } from 'react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppProvider>
+          <App />
+        </AppProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
